e5b6c04cb89df7ca470ef446d9aaa90707cf07e2
WeiKuoAn <qaz898970@mail.com>
2023-06-11 13:32:02 +0800

    1

--- resources/views/layouts/shared/topbar.blade.php
+++ resources/views/layouts/shared/topbar.blade.php
@@ -267,7 +267,7 @@
                 <a class="nav-link dropdown-toggle nav-user me-0 waves-effect waves-light" data-bs-toggle="dropdown" href="#" role="button" aria-haspopup="false" aria-expanded="false">
                     <img src="{{asset('assets/images/users/user-9.jpg')}}" alt="user-image" class="rounded-circle">
                     <span class="pro-user-name ms-1">
-                        {{ auth()->user()->name }} <i class="mdi mdi-chevron-down"></i>
+                        {{ Auth::user()->name }} <i class="mdi mdi-chevron-down"></i>
                     </span>
                 </a>
                 <div class="dropdown-menu dropdown-menu-end profile-dropdown ">
