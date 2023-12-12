44e3487f456d6e3b79f83beee2cb77b0a067337b
WeiKuoAn <qaz898970@mail.com>
2023-12-12 20:12:07 +0800

    20231212修改

--- app/Http/Controllers/PayDataController.php
+++ app/Http/Controllers/PayDataController.php
@@ -241,6 +241,7 @@ class PayDataController extends Controller
         $pay->save();
         // dd($request->pay_invoice_number);
         PayItem::where('pay_data_id', $id)->delete();
+        $user = User::where('id',Auth::user()->id)->first();
         if(isset($request->pay_data_date)){
             foreach($request->pay_data_date as $key=>$data){
                 $Pay_Item = new PayItem();
@@ -259,6 +260,11 @@ class PayDataController extends Controller
                 }else{
                     $Pay_Item->vender_id = null;
                 }
+                // if($user->job_id == '1' || $user->job_id == '2' || $user->job_id == '4'){
+                //     $Pay_Item->status = 1;
+                // }else{
+                //     $Pay_Item->status = 0;
+                // }
                 $Pay_Item->comment = $request->pay_text[$key];
                 $Pay_Item->save();
             }
