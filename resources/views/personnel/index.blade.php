0c40af69181076c4edacb1bd40a74147e1a6f72e
魏國安 <qaz898970@gmail.com>
2025-07-01 18:18:27 +0800

    人士列表修改

--- resources/views/personnel/index.blade.php
+++ resources/views/personnel/index.blade.php
@@ -100,7 +100,6 @@
                                                 <a href="javascript: void(0);" class="table-action-btn dropdown-toggle arrow-none btn btn-outline-secondary waves-effect" data-bs-toggle="dropdown" aria-expanded="false">動作 <i class="mdi mdi-arrow-down-drop-circle"></i></a>
                                                 <div class="dropdown-menu dropdown-menu-end">
                                                     <a class="dropdown-item" href="{{ route('user.edit',$user->id) }}"><i class="mdi mdi-clipboard-text-search me-2 font-18 text-muted vertical-middle"></i>查看個資</a>
-                                                    <a class="dropdown-item" href="{{ route('user.work.index',$user->id) }}"><i class="mdi mdi-clock me-2 font-18 text-muted vertical-middle"></i>出勤紀錄</a>
                                                     <a class="dropdown-item" href="{{ route('user.leave_day',$user->id) }}"><i class="mdi mdi-clipboard-text-search me-2 font-18 text-muted vertical-middle"></i>查看假單</a>
                                                     <a class="dropdown-item" href="{{ route('user.pay',$user->id) }}"><i class="mdi mdi-clipboard-text-search me-2 font-18 text-muted vertical-middle"></i>查看收支紀錄</a>
                                                 </div>
